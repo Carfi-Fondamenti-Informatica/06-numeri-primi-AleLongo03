4c04fb27ee22f02d49f494e9b77c59fea5aefc6f
AleLongo03 <116566037+AleLongo03@users.noreply.github.com>
2022-12-03 11:37:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,11 +1,11 @@
 bool num_prim(int n, int i)
 {
     int r;
-    if(1<2)
+    if (1 < 2)
         return true;
-    r=n%i;
-    if(r==0)
+    r = n % i;
+    if (r == 0)
         return false;
     else
-        r=num_prim(n,i-1);
+        r = num_prim(n, i - 1);
 }
